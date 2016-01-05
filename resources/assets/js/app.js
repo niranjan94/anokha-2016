f9b4fbf20bc57b8af5b5bd411502359d53421a5a
Niranjan Rajendran <niranjan94@yahoo.com>
2016-01-05 14:40:12 +0530

    Version 1.1

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,7 +1,42 @@
-var voila = new Voila();
+var lock = false;
+var count = 0;
+function pushWithoutStateChange(stateObject, title, url) {
+    lock = true;
+    History.pushState(stateObject, title, url);
+}
 
-$(".login-form").bindLogin("/");
-$(".signup-form").bindRegister("/");
+function replaceWithoutStateChange(stateObject, title, url) {
+    lock = true;
+    History.replaceState(stateObject, title, url);
+}
+
+function backWithoutStateChange() {
+    lock = true;
+    History.back();
+}
+
+var voila = new Voila();
+var user;
+
+var $loginRegisterButton = $(".login-register-button"),
+    $hiButton = $(".hi-button");
+if(voila.isLoggedIn()) {
+    user = voila.getUser();
+    $loginRegisterButton.fadeOut();
+    $hiButton.find(".name").text(user.name);
+    $hiButton.fadeIn();
+}
+
+$(".login-form").bindLogin("/", function (user) {
+    $loginRegisterButton.fadeOut();
+    $hiButton.find(".name").text(user.name);
+    $hiButton.fadeIn();
+    $("#logreg").modal("hide");
+});
+
+$(".signup-form").bindRegister("/login", function () {
+    $('.tab').trigger("click");
+});
 
 var $eventGroups = $("#event-groups");
 var $workshopDepartments = $("#workshop-departments");
@@ -12,12 +47,42 @@ var $workshopGrid = $("#workshop-grid");
 var $eventModal = $("#event-modal");
 var $workshopModal = $("#workshop-modal");
 
+var $eventsListModel = $("#events");
+var $workshopsListModel = $("#ws");
+
 $eventModal.find(".tab-content").css("height", $(window).height() * 0.58 + "px");
 $workshopModal.find(".tab-content").css("height", $(window).height() * 0.58 + "px");
 
+$eventGrid.on( 'arrangeComplete', function( event, filteredItems ) {
+    $eventGrid.attr("style", "position: relative; height:"+$(window).height() * 0.58 + "px !important"+";");
+    $eventGrid.getNiceScroll().resize();
+    $eventGrid.removeAttr("tabindex");
+});
+
+$workshopGrid.on( 'arrangeComplete', function( event, filteredItems ) {
+    $workshopGrid.attr("style", "position: relative; height:"+$(window).height() * 0.58 + "px !important"+";");
+    $workshopGrid.getNiceScroll().resize();
+    $workshopGrid.removeAttr("tabindex");
+});
+
+$eventGrid.niceScroll({
+    cursorwidth:"10px",
+    cursorborderradius: "0px",
+    cursoropacitymin: 0.5,
+    horizrailenabled: false,
+    cursorborder: "none"
+});
+
+$workshopGrid.niceScroll({
+    cursorwidth:"10px",
+    cursorborderradius: "0px",
+    cursoropacitymin: 0.5,
+    horizrailenabled: false,
+    cursorborder: "none"
+});
 
 function getEventsListItemMarkup(event, typeClass) {
-    var $item = $('<div class="square grid-item hvr-sweep-to-right '+typeClass+'" data-category="' + typeClass + '">' +
+    var $item = $('<div class="square grid-item hvr-sweep-to-right '+typeClass+' event-identifier-'+event.id+'" data-category="' + typeClass + '">' +
     '<div class="sqcontent">'+
     '<div class="sqtable">'+
     '<div class="sqtable-cell">'+
@@ -48,21 +113,46 @@ voila.listEvents(function (events) {
 
     $eventGrid.isotope({
         itemSelector: '.grid-item',
-        filter: '*'
+        filter: '*',
+        layoutMode: 'fitRows',
+        fitRows: {
+            gutter: 3
+        },
+        resizesContainer : false
     });
+    count = count + 1;
+    if(count == 2) {
+        stateChangeHandler();
+    }
 
 }, "event");
 
-voila.listEvents(function (events) {
+voila.listDepartments(function(departments){
+    $workshopDepartments.append(getEventsSortButtonMarkup("event-sorter active", "All Workshops", "*"));
+    $.each(departments, function( index, department ) {
+        $workshopDepartments.append(getEventsSortButtonMarkup("event-sorter", department.name, "department-" + department.id));
+    });
+}) ;
 
+voila.listEvents(function (events) {
     $.each(events, function( index, event ) {
         $workshopGrid.append(getEventsListItemMarkup(event, "department-" + event.department_id));
     });
 
     $workshopGrid.isotope({
         itemSelector: '.grid-item',
-        filter: '*'
+        filter: '*',
+        layoutMode: 'fitRows',
+        fitRows: {
+            gutter: 3
+        },
+        resizesContainer : false
     });
+
+    count = count + 1;
+    if(count == 2) {
+        stateChangeHandler();
+    }
 
 }, "workshop");
 
@@ -75,7 +165,8 @@ $eventGroups.on('click', 'li', function(){
     if(sortClass==".*") {
         sortClass = "*";
     }
-    $eventGrid.isotope({ filter: sortClass })
+
+    $eventGrid.isotope({ filter: sortClass });
 });
 
 $workshopDepartments.on('click', 'li', function(){
@@ -90,57 +181,73 @@ $workshopDepartments.on('click', 'li', function(){
     $workshopGrid.isotope({ filter: sortClass })
 });
 
-$('#events').on('shown.bs.modal', function (e) {
-    $eventGrid.isotope({ filter: "*" })
+$eventsListModel.on('shown.bs.modal', function (e) {
+    $eventGrid.isotope({ filter: "*" });
+    pushWithoutStateChange(null, "Events - Anokha 2016", "/events");
+}).on('hidden.bs.modal', function (e) {
+    History.replaceState(null, "Anokha 2016", "/");
 });
 
+$workshopsListModel.on('shown.bs.modal', function (e) {
+    $workshopGrid.isotope({ filter: "*" });
+    pushWithoutStateChange(null, "Workshops - Anokha 2016", "/workshops");
+}).on('hidden.bs.modal', function (e) {
+    History.replaceState(null, "Anokha 2016", "/");
+});
+
+var $eventDateHolder,$eventContactsHolder;
 $(document).on('click', '.grid-item', function(){
     var event = $(this).data("event");
     var $modal;
+
+    pushWithoutStateChange(null, event.name, "/"+event.type+"/"+event.slug+"/"+event.id);
+
     if(event.type == "event") {
         $modal = $eventModal;
-        $modal.find(".title").text(event.name);
-        $modal.find("#event-desc").html(event.description);
+        $modal.find("#event-prize").text("Rs. "+event.prize);
+        $modal.find("#event-rules").html(event.rules);
+        $modal.find("#event-judge").html(event.judgement_criteria);
 
-        if(event.join_type == "individual") {
-            $modal.find("#event-type").text("Individual");
-        } else {
-            if(event.group_info.max == event.group_info.min) {
-                $modal.find("#event-type").text("Group ("+ event.group_info.max + " members)");
-            } else {
-                $modal.find("#event-type").text("Group ("+ event.group_info.min + " to "+ event.group_info.max + " members)");
-            }
-
-            $modal.find("#event-prize").text("Rs. "+event.prize);
-
-            var $eventDateHolder = $modal.find("#event-date");
-            $eventDateHolder.text("");
-
-            $.each(event.dates_timing, function( index, date ) {
-                if(date.from == "" || date.to == "") {
-                    $eventDateHolder.append(index + "<br>")
-                } else {
-                    $eventDateHolder.append(index + " - " + date.from + " to " + date.to + "<br>")
-                }
-            });
-
-            $modal.find("#event-location").text(event.venue);
-
-            $modal.find("#event-fee").text(event.human_readable_fee);
-            $modal.find("#event-rules").html(event.rules);
-            $modal.find("#event-judge").html(event.judgement_criteria);
-            var $eventContactsHolder  = $modal.find("#event-contacts");
-
-            $.each(event.contact, function( index, contact ) {
-                if(contact.name != "" && contact.number != "") {
-                    var $contactItem = $("<li>"+contact.name + " - " +  contact.number+"</li>");
-                    $eventContactsHolder.append($contactItem)
-                }
-            });
-        }
     } else {
         $modal = $workshopModal;
+        $modal.find("#event-about").html(event.explanation);
     }
+
+    $modal.find(".title").text(event.name);
+    $modal.find("#event-desc").html(event.description);
+
+    if(event.join_type == "individual") {
+        $modal.find("#event-type").text("Individual");
+    } else {
+        if (event.group_info.max == event.group_info.min) {
+            $modal.find("#event-type").text("Group (" + event.group_info.max + " members)");
+        } else {
+            $modal.find("#event-type").text("Group (" + event.group_info.min + " to " + event.group_info.max + " members)");
+        }
+    }
+
+    $eventDateHolder = $modal.find("#event-date");
+    $eventDateHolder.text("");
+
+    $.each(event.dates_timing, function( index, date ) {
+        if(date.from == "" || date.to == "") {
+            $eventDateHolder.append(index + "<br>")
+        } else {
+            $eventDateHolder.append(index + " - " + date.from + " to " + date.to + "<br>")
+        }
+    });
+
+    $modal.find("#event-location").text(event.venue);
+    $modal.find("#event-fee").text(event.human_readable_fee);
+
+    $eventContactsHolder  = $modal.find("#event-contacts");
+    $eventContactsHolder.text("");
+    $.each(event.contact, function( index, contact ) {
+        if(contact.name != "" && contact.number != "") {
+            var $contactItem = $("<li>"+contact.name + " - " +  contact.number+"</li>");
+            $eventContactsHolder.append($contactItem)
+        }
+    });
 
     if(voila.isLoggedIn()) {
         $modal.find(".register-btn").data("event-id", event.id);
@@ -152,7 +259,50 @@ $(document).on('click', '.grid-item', function(){
     $modal.modal("show");
 });
 
+
+
 $(".register-btn").click(function () {
     var eventId = $(this).data("event-id");
     voila.registerForEvent(eventId);
 });
+
+
+$eventModal.on('hidden.bs.modal', function (e) {
+    History.replaceState(null, "Events - Anokha 2016", "/events");
+});
+
+$workshopModal.on('hidden.bs.modal', function (e) {
+    History.replaceState(null, "Workshops - Anokha 2016", "/workshops");
+});
+
+var stateChangeHandler = function(){
+    if(!lock) {
+        var path = new URI(window.location.href).pathname();
+        var splitPath = path.split("/");
+        if(path.startsWith("/event/")) {
+            $(".event-identifier-"+splitPath[splitPath.length-1]).trigger("click");
+            console.log(".event-identifier-"+splitPath[splitPath.length-1]);
+        } else if(path.startsWith("/workshop/")) {
+            $(".event-identifier-"+splitPath[splitPath.length-1]).trigger("click");
+        } else if(path.startsWith("/events")) {
+            $eventsListModel.modal("show");
+            $eventModal.modal("hide");
+            $workshopModal.modal("hide");
+        } else if(path.startsWith("/workshops")) {
+            $workshopsListModel.modal("show");
+            $eventModal.modal("hide");
+            $workshopModal.modal("hide");
+        } else if(path == "/") {
+            $eventModal.modal("hide");
+            $workshopModal.modal("hide");
+            $eventsListModel.modal("hide");
+            $workshopsListModel.modal("hide");
+        }
+    } else {
+        lock = false;
+    }
+};
+
+History.Adapter.bind(window,'statechange', stateChangeHandler);
+
+
--- resources/assets/js/vendor/animate.js
+++ resources/assets/js/vendor/animate.js
@@ -1,157 +1,195 @@
-var main = function(){
-  
-  $('.tab').click(function() {
-    $('#signup').css({
-      'display': 'none'});
-    $('#login').css({
-      'display': 'block'});
-    $('.tabactive').css({
-      'border':'none'});
-    $('.tab').css({
-      'border':'1px solid #252525' });
-  });
-  
-  $('.tabactive').click(function() {
-    $('#signup').css({
-      'display': 'block'});
-    $('#login').css({
-      'display': 'none'});
-    $(this).css({
-      'border':'1px solid #252525' });
-    $('.tab').css({
-      'border':'none'  });
-  });      
- 
-  $('#menu3').click(function() {
-    $('#events,#lumiere,#workshop,#retaliation,#expo,#leaders').css({
-      display:'none'});
-    $('#eventide').css({
-      display:'block'});
-    $('#menu3').css({
-      'border-bottom':'3px solid #FF7518'});
-    $('#menu6,#menu5,#menu4,#menu2,#menu1,#menu7').css({
-      'border-bottom':'none'});
-  });
-  
-  $('#menu4').click(function() {
-    $('#events,#workshops,#eventide,#retaliation,#expo,#leaders').css({
-      display:'none'});
-    $('#lumiere').css({
-      display:'block'});
-    $('#menu4').css({
-      'border-bottom':'3px solid #FF7518'});
-    $('#menu6,#menu5,#menu3,#menu2,#menu1,#menu7').css({
-     'border-bottom':'none'});
-  });
-  
-  $('#menu5').click(function() {
-    $('#events,#lumiere,#eventide,#retaliation,#workshops,#leaders').css({
-      display:'none'});
-    $('#expo').css({
-      display:'block'});
-    $('#menu5').css({
-      'border-bottom':'3px solid #FF7518'});
-    $('#menu6,#menu4,#menu3,#menu2,#menu1,#menu7').css({
-      'border-bottom':'none'});
-  });
-  
-  $('#menu6').click(function() {
-    $('#events,#lumiere,#eventide,#workshops,#expo,#leaders').css({
-      display:'none'});
-    $('#retaliation').css({
-      display:'block'});
-    $('#menu6').css({
-      'border-bottom':'3px solid #FF7518'});
-    $('#menu5,#menu4,#menu3,#menu2,#menu1,#menu7').css({
-      'border-bottom':'none'});
-  });
-  
-  $('#menu7').click(function() {
-    $('#events,#lumiere,#eventide,#retaliation,#workshops,#expo').css({
-      display:'none'});
-    $('#leaders').css({
-      display:'block'});
-    $('#menu7').css({
-      'border-bottom':'3px solid #FF7518'});
-    $('#menu6,#menu5,#menu4,#menu3,#menu2,#menu1').css({
-     'border-bottom':'none'});
-  });
-  
-  $('#menu1').click(function(){
-    $('.animation-element').animate({
-    left:'75px'}, 500);
-  });
-  
-  $('#menu7,#menu6,#menu5,#menu4,#menu3,#menu2').click(function() {
-    $('.animation-element').animate({
-    left:'-150%'}, 200);
-  });
-  
-  $('.leftmenu span').click(function(){
-    $('.leftmenu span').removeClass("cover");
-    $(this).addClass("cover");
-  });  
-  
-  $('.leftmenu span:nth-child(1)').click(function(){
-    var $container      = $('#ib-container'),
-      $articles       = $container.children('article'), timeout;
-      $articles.on( 'mouseenter', function( event ) {
-          var $article      = $(this);
-          clearTimeout( timeout );
-            timeout = setTimeout( function() {                    
-          if( $article.hasClass('active') ) return false;          
-          $articles.not( $article.removeClass('blur').addClass('active') )
-                           .removeClass('active')
-                           .addClass('blur');
-          }, 65 );
-      });
-      $container.on( 'mouseleave', function( event ) {
-        clearTimeout( timeout );
-        $articles.removeClass('active blur');
-      });
-  });
- 
-  $('#schedule').click(function(){
-    $('.schedule').css({
-      display:'block'});
-    $('.transport,.sponsors,.contacts').css({
-      display:'none'
+var main = function () {
+
+    $('.tab').click(function () {
+        $('#signup').css({
+            'display': 'none'
+        });
+        $('#login').css({
+            'display': 'block'
+        });
+        $('.tabactive').css({
+            'border': 'none'
+        });
+        $('.tab').css({
+            'border': '1px solid #252525'
+        });
     });
-  }); 
- 
-  $('#transport').click(function(){
-    $('.transport').css({
-      display:'block'});
-    $('.schedule,.sponsors,.contacts').css({
-      display:'none'
+
+    $('.tabactive').click(function () {
+        $('#signup').css({
+            'display': 'block'
+        });
+        $('#login').css({
+            'display': 'none'
+        });
+        $(this).css({
+            'border': '1px solid #252525'
+        });
+        $('.tab').css({
+            'border': 'none'
+        });
     });
-  });
-  
-  $('#sponsors').click(function(){
-    $('.sponsors').css({
-      display:'block'});
-    $('.schedule,.transport,.contacts').css({
-      display:'none'
+
+    $('#menu3').click(function () {
+        $('#events,#lumiere,#workshop,#retaliation,#expo,#leaders').css({
+            display: 'none'
+        });
+        $('#eventide').css({
+            display: 'block'
+        });
+        $('#menu3').css({
+            'border-bottom': '3px solid #FF7518'
+        });
+        $('#menu6,#menu5,#menu4,#menu2,#menu1,#menu7').css({
+            'border-bottom': 'none'
+        });
     });
-  });
-  
-  $('#contacts').click(function(){
-    $('.contacts').css({
-      display:'block'});
-    $('.schedule,.transport,.sponsors').css({
-      display:'none'
+
+    $('#menu4').click(function () {
+        $('#events,#workshops,#eventide,#retaliation,#expo,#leaders').css({
+            display: 'none'
+        });
+        $('#lumiere').css({
+            display: 'block'
+        });
+        $('#menu4').css({
+            'border-bottom': '3px solid #FF7518'
+        });
+        $('#menu6,#menu5,#menu3,#menu2,#menu1,#menu7').css({
+            'border-bottom': 'none'
+        });
     });
-  });
-  
-  $('.animation-element1').click(function(){
-    $('.animation-element1').css({
-      border:'none'
+
+    $('#menu5').click(function () {
+        $('#events,#lumiere,#eventide,#retaliation,#workshops,#leaders').css({
+            display: 'none'
+        });
+        $('#expo').css({
+            display: 'block'
+        });
+        $('#menu5').css({
+            'border-bottom': '3px solid #FF7518'
+        });
+        $('#menu6,#menu4,#menu3,#menu2,#menu1,#menu7').css({
+            'border-bottom': 'none'
+        });
     });
-    $(this).css({
-      'border-left': '4px solid #FF7518'
+
+    $('#menu6').click(function () {
+        $('#events,#lumiere,#eventide,#workshops,#expo,#leaders').css({
+            display: 'none'
+        });
+        $('#retaliation').css({
+            display: 'block'
+        });
+        $('#menu6').css({
+            'border-bottom': '3px solid #FF7518'
+        });
+        $('#menu5,#menu4,#menu3,#menu2,#menu1,#menu7').css({
+            'border-bottom': 'none'
+        });
     });
-  });
-  
+
+    $('#menu7').click(function () {
+        $('#events,#lumiere,#eventide,#retaliation,#workshops,#expo').css({
+            display: 'none'
+        });
+        $('#leaders').css({
+            display: 'block'
+        });
+        $('#menu7').css({
+            'border-bottom': '3px solid #FF7518'
+        });
+        $('#menu6,#menu5,#menu4,#menu3,#menu2,#menu1').css({
+            'border-bottom': 'none'
+        });
+    });
+
+    $('#menu1').click(function () {
+        $('.animation-element').animate({
+            left: '75px'
+        }, 500);
+    });
+
+    $('#menu7,#menu6,#menu5,#menu4,#menu3,#menu2').click(function () {
+        $('.animation-element').animate({
+            left: '-150%'
+        }, 200);
+    });
+
+    $('.leftmenu span').click(function () {
+        $('.leftmenu span').removeClass("cover");
+        $(this).addClass("cover");
+    });
+
+    $('.leftmenu span:nth-child(1)').click(function () {
+        var $container = $('#ib-container'),
+            $articles = $container.children('article'), timeout;
+        $articles.on('mouseenter', function (event) {
+            var $article = $(this);
+            clearTimeout(timeout);
+            timeout = setTimeout(function () {
+                if ($article.hasClass('active')) return false;
+                $articles.not($article.removeClass('blur').addClass('active'))
+                    .removeClass('active')
+                    .addClass('blur');
+            }, 65);
+        });
+        $container.on('mouseleave', function (event) {
+            clearTimeout(timeout);
+            $articles.removeClass('active blur');
+        });
+    });
+
+    $('#schedule').click(function () {
+        $('.schedule').css({
+            display: 'block'
+        });
+        $('.transport,.sponsors,.contacts').css({
+            display: 'none'
+        });
+    });
+
+    $('#transport').click(function () {
+        $('.transport').css({
+            display: 'block'
+        });
+        $('.schedule,.sponsors,.contacts').css({
+            display: 'none'
+        });
+    });
+
+    $('#sponsors').click(function () {
+        $('.sponsors').css({
+            display: 'block'
+        });
+        $('.schedule,.transport,.contacts').css({
+            display: 'none'
+        });
+    });
+
+    $('#contacts').click(function () {
+        $('.contacts').css({
+            display: 'block'
+        });
+        $('.schedule,.transport,.sponsors').css({
+            display: 'none'
+        });
+    });
+
+    $('.animation-element1').click(function () {
+        if ($(this).parent().parent().data("dismiss") != "modal") {
+            $('.animation-element1').css({
+                border: 'none'
+            });
+
+            $(this).css({
+                'border-left': '4px solid #FF7518'
+            });
+
+        }
+    });
+
 };
 
 $(document).ready(main);
